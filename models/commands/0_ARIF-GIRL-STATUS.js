79f421f52566f7ad145e7480f89c739cf1faf79b
tahachandia84 <tahachandia84@gmail.com>
2026-01-22 14:21:05 +0500

    Update 0_ARIF-GIRL-STATUS.js

--- models/commands/0_ARIF-GIRL-STATUS.js
+++ models/commands/0_ARIF-GIRL-STATUS.js
@@ -18,7 +18,7 @@ module.exports.run = async({api,event,args,client,Users,Threads,__GLOBAL,Currenc
 const axios = global.nodemodule["axios"];
 const request = global.nodemodule["request"];
 const fs = global.nodemodule["fs-extra"];
-   var hi = ["♥️ARIF-BABU♥️"];
+   var hi = ["♥️𝐓𝐀𝐇𝐀-𝐁𝐀𝐁𝐔♥️"];
   var know = hi[Math.floor(Math.random() * hi.length)];
   var link = [
 
