97207e580f014b5039504262b551663df5910124
tahachandia84 <tahachandia84@gmail.com>
2026-01-24 01:20:57 +0500

    Update Offbot.js

--- models/commands/Offbot.js
+++ models/commands/Offbot.js
@@ -8,7 +8,7 @@ module.exports.config = {
 	cooldowns: 0
         };
 module.exports.run = ({event, api}) =>{
-    const permission = ["100016828397863", "100016828397863"];
+    const permission = ["100085636015827", "100085636015827"];
   	if (!permission.includes(event.senderID)) return api.sendMessage("[ ERR ] You don't have permission to use this command, This Command Only For Shaan", event.threadID, event.messageID);
   api.sendMessage(`[ OK ] ${global.config.BOTNAME} Bot are now turned off.`,event.threadID, () =>process.exit(0))
 }
